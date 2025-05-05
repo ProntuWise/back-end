d4d3ed2ec11905261f05bf530f274294f8c93741
João <galhardojoaopedro@gmail.com>
2025-05-05 20:37:59 -0300

    Update user model to include new fields and user type enumeration

--- src/models/user.py
+++ src/models/user.py
@@ -1,11 +1,16 @@
 
-
-from sqlalchemy import Column, Integer, String
-from src.models.setup_db import Base
+from sqlalchemy import Column, Integer, String, Enum, Boolean, TIMESTAMP, func
+from scripts.base import Base
+from src.enums.user_type_enum import UserTypeEnum
 
 class User(Base):
-    __tablename__ = "users"
-    
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    email = Column(String, unique=True, index=True)
+    __tablename__ = "user"
+
+    user_id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(100), unique=True, nullable=False)
+    password = Column(String(255), nullable=False)
+    full_name = Column(String(255), nullable=False)
+    role = Column(Enum(UserTypeEnum), nullable=False)
+    email = Column(String(255))
+    is_active = Column(Boolean, default=True)
+    created_at = Column(TIMESTAMP, server_default=func.now())
