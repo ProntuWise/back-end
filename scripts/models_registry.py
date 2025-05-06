e607185d4f4a3f488b1b02d59c2abfb8846fc57c
João <galhardojoaopedro@gmail.com>
2025-05-05 21:52:24 -0300

    Adds the models_registry.py file with imports of essential models for managing users, patients, appointments, and related information.

---
+++ scripts/models_registry.py
@@ -0,0 +1,7 @@
+from src.models.user import User
+from src.models.patient import Patient
+from src.models.schedule import Schedule
+from src.models.patient_info import PatientInfo
+from src.models.doctor_patient import DoctorPatient
+from src.models.insurance import Insurance
+from scripts.base import Base
