6426d2e8a71b075cd2de4f8f832f6850a398256b
João <galhardojoaopedro@gmail.com>
2025-05-05 21:52:11 -0300

    Refactor the database connection, removing unnecessary dependencies and using the SessionLocal class to manage sessions.

--- scripts/db_connection.py
+++ scripts/db_connection.py
@@ -1,24 +1,8 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-import os
-from dotenv import load_dotenv
-load_dotenv()
+from scripts.session import SessionLocal, engine
 
-# Carrega a URL de conexão do banco
-connect = os.getenv("AIVEN_URL")
-
-# Caminho do certificado SSL
-caminho = './scripts/ca.pem'
-caminho_completo = os.path.abspath(os.path.join(os.getcwd(), caminho))
-
-# Cria o engine de conexão
-engine = create_engine(connect, echo=True, connect_args={'ssl': {'ca': caminho_completo}})
-
-# Cria uma classe para gerenciar as sessões
 class Conexao:
     def __init__(self):
-        self.Session = sessionmaker(bind=engine)
-        self.session = self.Session()
+        self.session = SessionLocal()
 
     def fecha_conexao(self):
         self.session.close()
