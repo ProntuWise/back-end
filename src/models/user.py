baadf350eec998e9e1acb258bc6a0a17273c50ac
João <galhardojoaopedro@gmail.com>
2025-05-22 18:31:42 -0300

    Updates the User model to correct the table name, adjusts the user_id to autoincrement, renames the full_name column to name , and modifies the timestamp function to func.current_timestamp().

--- src/models/user.py
+++ src/models/user.py
@@ -4,13 +4,13 @@ from scripts.base import Base
 from src.enums.user_type_enum import UserTypeEnum
 
 class User(Base):
-    __tablename__ = "user"
+    __tablename__ = 'User'
 
-    user_id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, primary_key=True, autoincrement=True)
     username = Column(String(100), unique=True, nullable=False)
     password = Column(String(255), nullable=False)
-    full_name = Column(String(255), nullable=False)
+    name = Column(String(255), nullable=False)
     role = Column(Enum(UserTypeEnum), nullable=False)
     email = Column(String(255))
     is_active = Column(Boolean, default=True)
-    created_at = Column(TIMESTAMP, server_default=func.now())
+    created_at = Column(TIMESTAMP, server_default=func.current_timestamp())
