83f491ab594114fea644068d561470c66a6c4cc2
João <galhardojoaopedro@gmail.com>
2025-05-22 18:27:46 -0300

    Updates the UserTypeEnum to replace 'Secretary' with 'Unique_Secretary' and add 'General_Secretary'.

--- src/enums/user_type_enum.py
+++ src/enums/user_type_enum.py
@@ -3,5 +3,6 @@ import enum
 class UserTypeEnum(enum.Enum):
     Doctor = 'Doctor'
     Admin = 'Admin'
-    Secretary = 'Secretary'
+    Unique_Secretary = 'Unique_Secretary'
+    General_Secretary = 'General_Secretary'
     Full = 'Full'
