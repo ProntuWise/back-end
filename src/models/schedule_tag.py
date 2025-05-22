7edba374b678ff1500e841d7d084ab0e1eddef2e
João <galhardojoaopedro@gmail.com>
2025-05-22 18:31:16 -0300

    Adds the ScheduleTag model with fields for identifying schedule tags, including a unique ID, name, and description.

---
+++ src/models/schedule_tag.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String, Text
+from scripts.base import Base
+
+class ScheduleTag(Base):
+    __tablename__ = 'ScheduleTag'
+
+    schedule_tag_id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100), unique=True, nullable=False)
+    description = Column(Text)
