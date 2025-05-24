8d7c3179ef7f9fda830e8b85c0eaa307b756f3e3
Gabriel Merola <gabrielmerola12@gmail.com>
2025-05-24 16:55:59 -0300

    Refactor user creation logic to utilize repository pattern based on development mode

--- src/routes/user_routes.py
+++ src/routes/user_routes.py
@@ -3,6 +3,12 @@ from fastapi.responses import JSONResponse
 
 from src.services.user_service import UserServices
 from src.entities.user import CreateUserRequest
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+dev_mode = os.getenv("DEV_MODE")
 
 router = APIRouter(
   prefix="/users",
@@ -16,8 +22,17 @@ def createUser(user: CreateUserRequest):
     if not user.name or not user.email or not user.role:
       return JSONResponse(status_code=422, content={"message": "Todos os campos são obrigatórios"})
     
+    # Repository
+    if dev_mode == "True":
+      from src.repositories.user_repository import UserRepository
+      repo = UserRepository()
+    else:
+      print("MOCK")
+      from src.mock.user_repository_mock import UserRepositoryMock
+      repo = UserRepositoryMock()
+    
     # Create user object
-    user = UserServices().createUser(user)
+    user = UserServices(repo).createUser(user)
     if not user:
       return JSONResponse(status_code=400, content={"message": "Erro ao criar usuário"})
     
