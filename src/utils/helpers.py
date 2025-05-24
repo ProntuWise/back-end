4111ecdb608f61676d1ca622be8301cb84172955
Gabriel Merola <gabrielmerola12@gmail.com>
2025-05-24 19:07:54 -0300

    Add detailed logging for database exceptions in handle_database_exception function

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -3,8 +3,11 @@ from fastapi import HTTPException
 
 def handle_database_exception(e: Exception, context: str = ""):
   if isinstance(e, IntegrityError):
+    print(f"IntegrityError in {context}: {e.orig}")
     raise HTTPException(status_code=409, detail={"message":"Conflito de integridade no banco de dados."})
   elif isinstance(e, SQLAlchemyError):
+    print(f"SQLAlchemyError in {context}: {e}")
     raise HTTPException(status_code=500, detail={"message":"Erro ao acessar o banco de dados."})
   else:
+    print(f"Unexpected error in {context}: {e}")
     raise HTTPException(status_code=500, detail={"message":"Erro inesperado ao processar a requisição."})
