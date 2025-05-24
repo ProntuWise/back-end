310602608aeec775ddc259002580f48404070b0a
Gabriel Merola <gabrielmerola12@gmail.com>
2025-05-24 16:56:08 -0300

    Add unit test for user creation in UserServices using UserRepositoryMock

--- src/tests/test_user.py
+++ src/tests/test_user.py
@@ -1,2 +1,25 @@
 def test_somar():
   assert 2 + 3 == 5
+
+def test_create_user():
+  from src.services.user_service import UserServices
+  from src.mock.user_repository_mock import UserRepositoryMock
+  from src.enums.user_type_enum import UserTypeEnum
+  from src.entities.user import User
+
+  # Mock the repository
+  repo = UserRepositoryMock()
+  user_service = UserServices(repo)
+  user_data = User(
+    name="Isaias Cano Bello da Luz",
+    email="isaias@gmail.com",
+    password="ajdbhfuy498u31br89341&*#Y$*@#oihfweo",
+    role=UserTypeEnum.Doctor,
+  )
+  user = user_service.createUser(user_data)
+
+  assert user is not None
+  assert user.name == "Isaias Cano Bello da Luz"
+  assert user.email == "isaias@gmail.com"
+  assert user.role.value == "Doctor"
+  assert user.password is not None
