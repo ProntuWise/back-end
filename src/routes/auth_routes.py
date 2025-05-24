82f04661453084b6bc83a4272180d82d925182bf
Gabriel Merola <gabrielmerola12@gmail.com>
2025-05-24 19:07:29 -0300

    Add auth_routes for user login functionality

---
+++ src/routes/auth_routes.py
@@ -0,0 +1,40 @@
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+
+from src.services.auth_service import AuthService
+from src.entities.user import LoginUserRequest
+from src.utils.helpers import handle_database_exception
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+dev_mode = os.getenv("DEV_MODE")
+
+router = APIRouter(
+  prefix="/auth",
+  tags=["auth"]
+)
+
+@router.post("")
+async def login(data: LoginUserRequest):
+  try:
+    if not data.identifier or not data.password:
+      return JSONResponse(status_code=422, content={"message": "Todos os campos são obrigatórios"})
+
+    # Repository
+    if dev_mode == "True":
+      from src.repositories.user_repository import UserRepository
+      repo = UserRepository()
+    else:
+      from src.mock.user_repository_mock import UserRepositoryMock
+      repo = UserRepositoryMock()
+    
+    # Service
+    login = AuthService(repo).login(
+      identifier=data.identifier,
+      password=data.password
+    )
+    return JSONResponse(status_code=200, content={"token": login})
+  except Exception as e:
+    handle_database_exception(e, "LoginUser")
