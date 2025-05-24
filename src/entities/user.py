098fedc9ff28756178b9c2f1a13db1eb09179c54
Gabriel Merola <gabrielmerola12@gmail.com>
2025-05-24 16:55:42 -0300

    Refactor User class for improved validation and initialization logic

--- src/entities/user.py
+++ src/entities/user.py
@@ -1,44 +1,49 @@
 from pydantic import BaseModel
-
+from datetime import datetime
 from src.enums.user_type_enum import UserTypeEnum
 
-class User():
-  def __init__ (self, name: str, email: str, password: str, role: UserTypeEnum):
-    if self.validateName(name):
-      self.name = name,
-      nameList = name.split(" ")
-      self.username = nameList[0].strip().upper() + nameList[-1].lower()
-    if self.validateEmail(email):
-      self.email = email
-    if self.validateRole(role):
-      self.role = role
-    self.password = password
-    self.is_active = True
-    self.is_first = True
+class User:
+    def __init__(self, name: str, email: str, password: str, role: UserTypeEnum):
+        # Validações
+        self.validateName(name)
+        self.validateEmail(email)
+        self.validateRole(role)
 
-  def validateName(self, name: str):
-    if len(name) < 3:
-      raise ValueError("Nome tem que ter mais de 3 caracteres")
-    if name.isalpha():
-      raise ValueError("Nome não pode conter números")
-    return True
-  
-  def validateEmail(self, email: str):
-    if len(email) < 3:
-      raise ValueError("Email tem que ter mais de 3 caracteres")
-    if "@" not in email:
-      raise ValueError("Email tem que conter @")
-    if "." not in email:
-      raise ValueError("Email tem que conter .")
-    return True
-  
-  def validateRole(self, role: UserTypeEnum):
-    if role not in UserTypeEnum:
-      raise ValueError("Tipo de usuário inválido")
-    return True
+        self.name = name
+        name_parts = name.strip().split(" ")
+
+        if len(name_parts) > 1:
+            self.username = name_parts[0].strip().upper() + name_parts[-1].lower()
+        else:
+            self.username = name_parts[0].strip().upper()
+
+        self.email = email
+        self.password = password
+        self.role = role
+        self.is_active = True
+        self.is_first = True
+
+    def validateName(self, name: str):
+        if len(name) < 3:
+            raise ValueError("Nome deve ter pelo menos 3 caracteres")
+        if any(char.isdigit() for char in name):
+            raise ValueError("Nome não pode conter números")
+        return True
+
+    def validateEmail(self, email: str):
+        if len(email) < 3:
+            raise ValueError("Email deve ter pelo menos 3 caracteres")
+        if "@" not in email or "." not in email:
+            raise ValueError("Email inválido")
+        return True
+
+    def validateRole(self, role: UserTypeEnum):
+        if not isinstance(role, UserTypeEnum):
+            raise ValueError("Tipo de usuário inválido")
+        return True
 
 
 class CreateUserRequest(BaseModel):
-  name: str
-  role: UserTypeEnum
-  email: str
+    name: str
+    role: UserTypeEnum
+    email: str
