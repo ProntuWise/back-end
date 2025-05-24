de1dea0ff872b001fd56f236b1996c2d73f3792b
Gabriel Merola <gabrielmerola12@gmail.com>
2025-05-24 16:18:42 -0300

    Adds is_first field to User model for tracking first-time user status

--- src/models/user.py
+++ src/models/user.py
@@ -1,6 +1,7 @@
 
 from sqlalchemy import Column, Integer, String, Enum, Boolean, TIMESTAMP, func
 from scripts.base import Base
+
 from src.enums.user_type_enum import UserTypeEnum
 
 class User(Base):
@@ -12,5 +13,6 @@ class User(Base):
     name = Column(String(255), nullable=False)
     role = Column(Enum(UserTypeEnum), nullable=False)
     email = Column(String(255))
+    is_first = Column(Boolean, default=True)
     is_active = Column(Boolean, default=True)
     created_at = Column(TIMESTAMP, server_default=func.current_timestamp())
