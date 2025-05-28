4a0b74faf87d92cb959e72f3219fda31f6b4d56f
João <galhardojoaopedro@gmail.com>
2025-05-27 22:27:48 -0300

    Adds schemas for user creation and login, including field and data type validation.

---
+++ src/schemas/user_schema.py
@@ -0,0 +1,15 @@
+
+
+from pydantic import BaseModel
+
+from src.enums.user_type_enum import UserTypeEnum
+
+
+class CreateUserRequest(BaseModel):
+    name: str
+    role: UserTypeEnum
+    email: str
+
+class LoginUserRequest(BaseModel):
+    identifier: str
+    password: str
